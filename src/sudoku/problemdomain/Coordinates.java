e86f270985c06b94a65ad345fb78b75b46e967d2
2miles <miles4599@gmail.com>
2021-09-20 03:03:09 -0700

    added hashCode to Coordinates

--- src/sudoku/problemdomain/Coordinates.java
+++ src/sudoku/problemdomain/Coordinates.java
@@ -1,2 +1,52 @@
-package sudoku.problemdomain;public class Coordinates {
+package sudoku.problemdomain;
+
+import java.util.Objects;
+
+public class Coordinates {
+    private final int x;
+    private final int y;
+
+
+    //Constructor
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+
+
+    //We want to be able to store these objects in a hash map
+    //basically as keys to keep track of different UI elements
+    @Override
+    public boolean equals(Object o){
+        //TRUE If they have same address.
+        //FALSE If arg is null or not same type.
+        //TRUE if both x,y are the same, ELSE FALSE
+        if (this == o) {
+            return true; }
+        if (o == null || getClass() != o.getClass()) {
+            return false; }
+        Coordinates that = (Coordinates) o;
+        return x == that.x && y == that.y;
+    }
+
+
+    //hashCode is a unique identifier that is generated from
+    //some data you give it
+    @Override
+    public int hashCode(){
+       //generate a unique identifier from specific x,y values of a Coordinate
+       return Objects.hash(x,y);
+    }
+
+
+
 }
