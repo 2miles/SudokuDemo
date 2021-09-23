f477edf21416343ac734bf545aa042328e2004d6
2miles <miles4599@gmail.com>
2021-09-23 08:22:56 -0700

    added a README

---
+++ README.md
@@ -0,0 +1,20 @@
+##Sudoku Game Tutorial
+I copied, step by step, the code for a sudoku game. 
+This tutorial is on Youtube from freeCodeCamp.org. 
+The code is in Java and uses JavaFx for the GUI.
+
+https://www.youtube.com/watch?v=qH9mWpYMtYU&t=2145s
+
+My goal with this project is to become more familiar with Java and OOP
+through exposure to quality UI design and simple game logic.
+This is purely a learning tool. The code is very heavily commented.
+Most of this is info I found valuable from the creator as they 
+verbalized their thought process during the coding.
+
+###Things I Learned about
+
+- JavaFx
+- Hash Maps
+- Interfaces
+-  
+
--- src/sudoku/userinterface/UserInterfaceImpl.java
+++ src/sudoku/userinterface/UserInterfaceImpl.java
@@ -1,4 +1,8 @@
 package sudoku.userinterface;
+import javafx.geometry.Pos;
+import javafx.scene.layout.Background;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 import sudoku.problemdomain.SudokuGame;
 import sudoku.problemdomain.Coordinates;
 
@@ -58,14 +62,152 @@ public class UserInterfaceImpl implements IUserInterfaceContract.View,
     private static final Color BOARD_BACKGROUND_COLOR = Color.rgb(224,242,241);
     private static final String SUDOKU = "Sudoku";
 
+    //Constructor
 
     public UserInterfaceImpl(Stage stage) {
         this.stage = stage;
+        this.root = new Group();
+        this.textFieldCoordinates = new HashMap<>();
+        initializeUserInterface(); //helper method, to avoid giant methods
     }
 
+    //for every user interface component im going to create a helper method
+    private void initializeUserInterface() {
+        drawBackground(root);
+        drawTitle(root);
+        drawSudokuBoard(root);
+        drawTextFields(root);
+        drawGridLines(root);
+
+        //whats going to actually reveal the ui after everything has been
+        //drawn and configured
+        stage.show();
+    }
+
+
+    //some gridLines are thicker than others
+    private void drawGridLines(Group group) {
+        int xAndY  = 114;
+        int index  = 0;
+
+        while (index < 8) {
+            int thickness;
+            if (index == 2 || index == 5 ) {
+                thickness = 3;
+            } else {
+                thickness = 2;
+            }
+
+            Rectangle verticalLine = getLine(
+                    xAndY + 64 * index,
+                    BOARD_PADDING,
+                    BOARD_X_AND_Y,
+                    thickness );
+
+
+            Rectangle horizontalLine = getLine(
+                    BOARD_PADDING,
+                    xAndY + 64 * index,
+                    thickness,
+                    BOARD_X_AND_Y
+            );
+
+            //how we actually ass the ui elements to the group
+            //he saddle function means se can slap any function in
+            //the super class of the ui elemts is a node
+            //so we can add a ny valid class that is a node
+            root.getChildren().addAll(verticalLine, horizontalLine);
+
+            ++index;
+
+        }
+
+    }
+    private Rectangle getLine ( double x, double y,
+                                double height, double width) {
+        Rectangle line = new Rectangle();
+        line.setX(x);
+        line.setY(y);
+        line.setHeight(height);
+        line.setWidth(width);
+        line.setFill(Color.BLACK);
+        return line;
+    }
+
+    private  void drawTextFields(Group root) {
+        final int xOrigin = 50;
+        final int yOrigin = 50;
+
+        final int xAndYDelta = 64;
+
+        //O(n^2) Runtime Complexity
+
+        //iterate through each of the 81 'cells' on the board
+        for (int xIndex = 0; xIndex < 9; xIndex++) {
+            for (int yIndex = 0; yIndex < 9; ++yIndex) {
+
+                //get location (x,y of top left corner) of each cell
+                int x = xOrigin + xIndex * xAndYDelta;
+                int y = yOrigin + yIndex * xAndYDelta;
+                //create text field for each 'cell'
+                SudokuTextField tile = new SudokuTextField(xIndex, yIndex);
+                styleSudokuTile(tile, x, y);
+
+                //this implements EventHandler
+                tile.setOnKeyPressed(this);
+
+                textFieldCoordinates.put(new Coordinates(xIndex, yIndex), tile);
+
+                root.getChildren().add(tile);
+            }
+        }
+    }
+
+
+    private void styleSudokuTile(SudokuTextField tile, double x, double y) {
+        Font numberFont = new Font(32);
+
+        tile.setFont(numberFont);
+        tile.setAlignment(Pos.CENTER);
+
+        tile.setLayoutX(x);
+        tile.setLayoutY(y);
+        tile.setPrefHeight(64);
+        tile.setPrefWidth(64);
+
+        tile.setBackground(Background.EMPTY);
+    }
+
+    private  void drawSudokuBoard(Group root) {
+        Rectangle boardBackground = new Rectangle();
+        boardBackground.setX(BOARD_PADDING);
+        boardBackground.setY(BOARD_PADDING);
+
+        boardBackground.setWidth(BOARD_X_AND_Y);
+        boardBackground.setHeight(BOARD_X_AND_Y);
+
+        boardBackground.setFill(BOARD_BACKGROUND_COLOR);
+
+        root.getChildren().addAll(boardBackground);
+    }
+
+    private void  drawTitle(Group root) {
+        Text title = new Text(235, 690, SUDOKU);
+        title.setFill(Color.WHITE);
+        Font titleFont = new Font(43);
+        title.setFont(titleFont);
+        root.getChildren().add(title);
+
+    }
+
+    private void  drawBackground(Group root) {
+    }
+
+    //we will have our BuildLogic, which will assign our control
+    //logic class to this particuluar view, obviously through the interface.
     @Override
     public void setListener(IUserInterfaceContract.EventListener listener) {
-
+        this.listener = listener;
     }
 
     @Override
