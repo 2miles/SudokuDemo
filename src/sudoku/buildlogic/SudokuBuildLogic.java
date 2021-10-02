2785723638f97e3623a5800aa351d1fd22c1b0e5
2miles <miles4599@gmail.com>
2021-10-01 21:53:54 -0700

    finnished LocalStorageImpl, which finishes the app

--- src/sudoku/buildlogic/SudokuBuildLogic.java
+++ src/sudoku/buildlogic/SudokuBuildLogic.java
@@ -1,11 +1,11 @@
 package sudoku.buildlogic;
 
-// Applying the principal of separation of configuration from use.
+// Applying the principal of separating the configuration from the use.
 // i.e.:
 // An object which uses other objects should not also build those objects,
 // You should pull the logic required to build those objects into a different class or method.
 // So that's basically whats going on here,
-// Were taking the logic required to wire up all the different actors in our application togeter
+// Were taking the logic required to wire up all the different actors in our application together
 // just before they launch for the user to use.
 // amd  were encapsulating that into an special little area in the application and
 // The good thing about using interfaces is that we can use these interfaces within
@@ -13,6 +13,7 @@ package sudoku.buildlogic;
 // its interface, same with storage, etc..
 
 import sudoku.computationlogic.GameLogic;
+import sudoku.persistence.LocalStorageImpl;
 import sudoku.problemdomain.IStorage;
 import sudoku.problemdomain.SudokuGame;
 import sudoku.userinterface.IUserInterfaceContract;
@@ -22,7 +23,7 @@ import java.io.IOException;
 
 // What this class does, is we attempt to get the gameData from storage, if it exists. If it
 // doesn't exit, the storage class will throw an IOException. If thats the case then we just
-// ask gameLogic for a new game, update storage, if possible, and
+// ask gameLogic for a new game, update storage, if possible, and then
 public class SudokuBuildLogic {
 
     public  static void build(IUserInterfaceContract.View userInterface) throws IOException {
@@ -35,7 +36,9 @@ public class SudokuBuildLogic {
             initialState = GameLogic.getNewGame();
             storage.updateGameData(initialState);
         }
-        IUserInterfaceContract.EventListener uiLogic = new ControlLogic(storage, userInterface);
+
+        IUserInterfaceContract.EventListener uiLogic =
+                new ControlLogic(storage, userInterface);
         userInterface.setListener(uiLogic);
         userInterface.updateBoard(initialState);
     }
